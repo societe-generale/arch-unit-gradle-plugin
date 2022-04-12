d9eceb7605fe0f0101174caf00d8922b85994dcc
Vincent FUCHS <vincent.fuchs@gmail.com>
2022-04-12 22:54:30 +0200

    upgrading to latest Maven + replacing TravisCI by Github actions

--- release/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_f5dbad37b2da_key -iv $encrypted_f5dbad37b2da_iv -in my.travis.gpg.enc -out my.travis.gpg -d
-  ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
-fi
