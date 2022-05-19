eb7a6ea5b99f13570d32725fd8c8b3e785b35901
Vincent FUCHS <vincent.fuchs@gmail.com>
2022-05-19 21:50:37 +0200

    passing the whole config as param

--- src/main/java/com/societegenerale/commons/plugin/gradle/WorkerAction.java
+++ src/main/java/com/societegenerale/commons/plugin/gradle/WorkerAction.java
@@ -1,13 +1,12 @@
 package com.societegenerale.commons.plugin.gradle;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
-import com.societegenerale.commons.plugin.model.Rules;
 import com.societegenerale.commons.plugin.service.RuleInvokerService;
 import org.apache.commons.lang3.StringUtils;
 import org.gradle.api.GradleException;
 import org.gradle.workers.WorkAction;
+import org.slf4j.LoggerFactory;
 
 public abstract class WorkerAction implements WorkAction<WorkerActionParams> {
 
@@ -15,24 +14,22 @@ public abstract class WorkerAction implements WorkAction<WorkerActionParams> {
 
     @Override
     public void execute() {
-        RuleInvokerService ruleInvokerService = getParameters().getRuleInvokerService().get();
-        Rules rules = getParameters().getRules().get();
+
+        ArchUnitGradleConfig archUnitGradleConfig = getParameters().getArchUnitGradleConfig().get();
+
+        RuleInvokerService ruleInvokerService = new RuleInvokerService(new GradleLogAdapter(LoggerFactory.getLogger(RuleInvokerService.class)),
+                new GradleScopePathProvider(archUnitGradleConfig),
+                archUnitGradleConfig.getExcludedPaths());
 
         String ruleFailureMessage = "";
 
         try {
 
-            ruleFailureMessage = ruleInvokerService.invokeRules(rules);
+            ruleFailureMessage = ruleInvokerService.invokeRules(archUnitGradleConfig.getRules());
 
             if (!StringUtils.isEmpty(ruleFailureMessage)) {
                 throw new GradleException(PREFIX_ARCH_VIOLATION_MESSAGE + " \n" + ruleFailureMessage);
             }
-    /*
-            Class<?> c = Thread.currentThread().getContextClassLoader().loadClass(className);
-            Method greetingMethod = c.getMethod("getGreeting");
-            Object o = c.newInstance();
-            System.out.println(greetingMethod.invoke(o));
-     */
 
         } catch ( InstantiationException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
--- src/main/java/com/societegenerale/commons/plugin/gradle/WorkerActionParams.java
+++ src/main/java/com/societegenerale/commons/plugin/gradle/WorkerActionParams.java
@@ -1,11 +1,8 @@
 package com.societegenerale.commons.plugin.gradle;
 
-import com.societegenerale.commons.plugin.model.Rules;
-import com.societegenerale.commons.plugin.service.RuleInvokerService;
 import org.gradle.api.provider.Property;
 import org.gradle.workers.WorkParameters;
 
 public interface WorkerActionParams extends WorkParameters {
-     Property<RuleInvokerService> getRuleInvokerService();
-    Property<Rules> getRules();
+    Property<ArchUnitGradleConfig> getArchUnitGradleConfig();
 }
