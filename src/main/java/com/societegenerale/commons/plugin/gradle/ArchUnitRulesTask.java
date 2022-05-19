e577e4b9eccdc687ea8f3cdebfc0e90463228a6c
Vincent FUCHS <vincent.fuchs@gmail.com>
2022-05-19 21:07:40 +0200

    it builds...

--- src/main/java/com/societegenerale/commons/plugin/gradle/ArchUnitRulesTask.java
+++ src/main/java/com/societegenerale/commons/plugin/gradle/ArchUnitRulesTask.java
@@ -6,13 +6,25 @@ import com.societegenerale.commons.plugin.service.RuleInvokerService;
 import org.apache.commons.lang3.StringUtils;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
+import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.tasks.Classpath;
+import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.workers.WorkQueue;
+import org.gradle.workers.WorkerExecutor;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.lang.reflect.InvocationTargetException;
 
-public class ArchUnitRulesTask extends DefaultTask {
+public abstract class ArchUnitRulesTask extends DefaultTask {
+
+    @InputFiles
+    @Classpath
+    public abstract ConfigurableFileCollection getClasspath();
+
+    @Inject
+    protected abstract WorkerExecutor getWorkerExecutor();
 
     private ArchUnitGradleConfig archUnitGradleConfig;
 
@@ -39,17 +51,26 @@ public class ArchUnitRulesTask extends DefaultTask {
             throw new GradleException("Arch unit Gradle Plugin should have at least one preconfigured/configurable rule!");
         }
 
-        String ruleFailureMessage = "";
+
 
         RuleInvokerService ruleInvokerService = new RuleInvokerService(new GradleLogAdapter(LoggerFactory.getLogger(RuleInvokerService.class)),
                                                                         new GradleScopePathProvider(archUnitGradleConfig),
                                                                         archUnitGradleConfig.getExcludedPaths());
-      
+
+        WorkQueue queue = getWorkerExecutor().classLoaderIsolation(spec -> spec.getClasspath().from(getClasspath()));
+        queue.submit(WorkerAction.class, params -> {
+                params.getRuleInvokerService().set(ruleInvokerService);
+                params.getRules().set(rules);
+        });
+
+/*
         ruleFailureMessage = ruleInvokerService.invokeRules(rules);
 
         if (!StringUtils.isEmpty(ruleFailureMessage)) {
             throw new GradleException(PREFIX_ARCH_VIOLATION_MESSAGE + " \n" + ruleFailureMessage);
         }
+
+ */
     }
 
 }
